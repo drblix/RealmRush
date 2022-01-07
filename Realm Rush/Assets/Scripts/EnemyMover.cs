25e0904e0915d96005cdae648493de4992f4bd2f
drblix <drblix75@gmail.com>
2022-01-07 13:44:24 -0500

    In progress

--- Realm Rush/Assets/Scripts/EnemyMover.cs
+++ Realm Rush/Assets/Scripts/EnemyMover.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
-    private void Awake()
+    [SerializeField] 
+    List<Waypoint> path = new List<Waypoint>();
+
+    [SerializeField]
+    private float waitTime;
+
+    private void Start()
     {
-        
+        StartCoroutine(FollowPath());
+    }
+
+    private IEnumerator FollowPath()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            Debug.Log(waypoint.name);
+
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
     }
 }
