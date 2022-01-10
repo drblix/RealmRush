cd0c96d491eb9491d978f50be6ee065c20ed6bc0
drblix <drblix75@gmail.com>
2022-01-10 14:04:13 -0500

    Finished path, enemy, and placing turrets

---
+++ Realm Rush/Assets/Scripts/TargetLocator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLocator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _weapon;
+    [SerializeField]
+    private Transform _target;
+
+    private void Start()
+    {
+        _target = FindObjectOfType<EnemyMover>().transform;
+    }
+
+    private void Update()
+    {
+        AimWeapon();
+    }
+
+    private void AimWeapon()
+    {
+        _weapon.LookAt(_target);
+
+    }
+}
